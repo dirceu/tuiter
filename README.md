5df9d5e8c5638eacb4db32ddb79b66fa5a4f9883
Dirceu Pereira Tiegs <dirceutiegs@gmail.com>
2017-05-21 09:26:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tuiter, an Electron client for Twitter Lite
+
+This is just a test to play around with Electron.
---
+++ main.js
@@ -0,0 +1,29 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 455, height: 755})
+  mainWindow.loadURL('https://mobile.twitter.com')
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
